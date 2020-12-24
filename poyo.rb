db97401c15ac4b6fc83753c90953172a553bec8d
pyon00 <love.sm.te.nandeyanen@gmail.com>
2020-12-25 00:53:25 +0900

    ４章を終えました

--- poyo.rb
+++ poyo.rb
@@ -4,3 +4,19 @@ puts :name.inspect #鍵が表示されるなどよりメタい情報が表示さ
 
 a = {one:'uno',two:'dos',three:'tres'}
 
+s = String.new("foobar")
+s = String.new "foobar"
+puts 引数,引数
+#クラス（String) はレシピ。コンタクトラクタは調理人。
+#コンストラクタはクラスに含まれている。これはオブジェクト指向。
+#関数の書き方はすべて統一されている。関数.new(引数,引数,,,)
+
+person1= {first:"あすか",last:"紅"}
+person2= {first:"あかり",last:"樫"}
+person3= {first:"りょうた",last:"紫"} 
+params={} # 'params' というハッシュを定義する ('parameters' の略)。
+params[:father] = person1
+params[:mother] = person2
+params[:child] = person3
+
+user= {name:,email:,password_digest:}
