6c6cb6d29a9ffdf492cd4b67763c457aa9f5b922
pyon00 <love.sm.te.nandeyanen@gmail.com>
2021-01-06 20:09:39 +0900

    Rails tyu-toriaru 7.1.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])  #paramsで:idパラメータを受け取る(/users/1にアクセスしたら1を受け取る)。DBからid:1のレコードをfindメソッドで取り出す
+  end
+
   def new
   end
 end
