77732b6078d28e5f0f05a7fa2f26465de1d4c461
pyon00 <love.sm.te.nandeyanen@gmail.com>
2021-01-22 19:17:50 +0900

    8.2.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save                        #保存に成功した場合
+      log_in @user
       flash[:success] = "ようこそ!!"      #flash=登録完了後のみウェルカムメッセージは表示させ、リロードしたら消えるようにする
       redirect_to @user                  #リダイレクト redirect_to user_url(@user)→redirect_to user_url(id: @user.id)
     else
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,21 +5,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-  test "invalid signup information" do                                          # 新規登録が失敗（フォーム送信が）した時用のテスト
-    get signup_path                                                             # ユーザー登録ページにアクセス
-    assert_no_difference 'User.count' do                                        # User.countでユーザー数が変わっていなければ（ユーザー生成失敗）true,変わっていればfalse
-      post users_path, params: { user: { name: "",                              # signup_pathからusers_pathに対してpostリクエスト送信(/usersへ)、paramsでuserハッシュとその下のハッシュで値を受け取れるか確認
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+  test "valid signup information" do                                            # 新規登録が成功（フォーム送信）したかのテスト
+    get signup_path                                                             # signup_path(/signup)ユーザー登録ページにアクセス
+    assert_difference 'User.count', 1 do                                        # User.countでユーザー数をカウント、1とし、ユーザー数が変わったらtrue、変わってなければfalse
+      post users_path, params: { user: { name:                  "Example User", # signup_path(/signup)からusers_path(/users)へparamsハッシュのuserハッシュの値を送れるか検証
+                                         email:                 "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!                                                            # 指定されたリダイレクト先(users/show)へ飛べるか検証
+    assert_template 'users/show'                                                # users/showが描画されているか確認
+    assert_not   flash.blank?                                                   # flashが空ならfalse,空じゃなければtrue
+    assert is_logged_in?                                                        # 新規登録時にセッションが空じゃなければtrue
   end
-
-  
-
-  assert_template 'users/new'                                                   # newアクションが描画(つまり@user.save失敗)されていればtrue、なければfalse
-  assert_select   'div#error_explanation'                                       # divタグの中のid error_explanationをが描画されていれば成功
-  assert_select   'div.field_with_errors'                                       # divタグの中のclass field_with_errorsが描画されていれば成功
-  
-  
-end
 end
