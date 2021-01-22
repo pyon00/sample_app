893c8766fd2a0a0240b2f4f3cd77528eaf56cd2e
pyon00 <love.sm.te.nandeyanen@gmail.com>
2021-01-22 18:05:27 +0900

    8.2.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,15 +1,20 @@
 module SessionsHelper
 
-    # 渡されたユーザーでログインする
-    def log_in(user)                                                              # login_inメソッドにuser(ログイン時にユーザーが送ったメールとパス)を引数として渡す
-      session[:user_id] = user.id                                                 # ユーザーidをsessionのuser_idに代入（ログインidの保持）
-    end
+ # 渡されたユーザーでログインする
+ def log_in(user)                                                              # login_inメソッドにuser(ログイン時にユーザーが送ったメールとパス)を引数として渡す
+    session[:user_id] = user.id                                                 # ユーザーidをsessionのuser_idに代入（ログインidの保持）
+  end
 
-      # 現在ログイン中のユーザーを返す (いる場合)
+  # 現在ログイン中のユーザーを返す(いる場合)
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if session[:user_id]                                                        # ログインユーザーがいればtrue処理
+      @current_user ||= User.find_by(id: session[:user_id])                     # ログインユーザーがいればそのまま、いなければcookiesのユーザーidと同じidを持つユーザーをDBから探して@current_user（現在のログインユーザー）に代入
     end
   end
-  
+
+  # ユーザーがログインしていればtrue、それ以外ならfalse
+  def logged_in?
+    !current_user.nil?                                                          # current_user(ログインユーザー)がnilじゃないならtrue、それ以外ならfalseを返す
+  end
+
   end
